38ce599f9d848296511f65987f439fed0b7d867c
Cameron Redovian <naivoder@gmail.com>
2024-06-21 18:54:17 -0400

    intial commit with SAC implementation

---
+++ agent.py
@@ -0,0 +1,186 @@
+import torch
+import numpy as np
+import networks
+import memory
+
+
+class SACAgent(torch.nn.Module):
+    def __init__(
+        self,
+        input_dims,
+        action_space,
+        tau=5e-3,
+        reward_scale=2,
+        batch_size=256,
+        lr=3e-4,
+        gamma=0.99,
+        h1_size=256,
+        h2_size=256,
+        mem_size=int(1e6),
+    ):
+        super(SACAgent, self).__init__()
+        self.input_dims = input_dims
+        self.n_actions = action_space.shape[0]
+        self.min_action = action_space.low
+        self.max_action = action_space.high
+        self.batch_size = batch_size
+        self.reward_scale = reward_scale
+        self.tau = tau
+        self.lr = lr
+        self.gamma = gamma
+        self.h1_size = h1_size
+        self.h2_size = h2_size
+        self.mem_size = mem_size
+
+        self.memory = memory.ReplayBuffer(
+            self.input_dims, self.n_actions, self.mem_size
+        )
+
+        self.Q1 = networks.CriticNetwork(
+            self.input_dims,
+            self.n_actions,
+            self.h1_size,
+            self.h2_size,
+            learning_rate=self.lr,
+            chkpt_path="weights/critic_1.pt",
+        )
+        self.Q2 = networks.CriticNetwork(
+            self.input_dims,
+            self.n_actions,
+            self.h1_size,
+            self.h2_size,
+            learning_rate=self.lr,
+            chkpt_path="weights/critic_2.pt",
+        )
+        self.V = networks.ValueNetwork(
+            self.input_dims,
+            self.h1_size,
+            self.h2_size,
+            learning_rate=self.lr,
+            chkpt_path="weights/value.pt",
+        )
+        self.V_target = networks.ValueNetwork(
+            self.input_dims,
+            self.h1_size,
+            self.h2_size,
+            learning_rate=self.lr,
+            chkpt_path="weights/value_target.pt",
+        )
+        self.Actor = networks.ActorNetwork(
+            self.input_dims,
+            self.n_actions,
+            self.h1_size,
+            self.h2_size,
+            learning_rate=self.lr,
+            max_action=self.max_action,
+            chkpt_path="weights/actor.pt",
+        )
+
+        self.update_network_params(tau=1)
+
+    def choose_action(self, state):
+        self.Actor.eval()
+        state = torch.tensor(np.array(state)).to(torch.float32).to(self.Actor.device)
+        action, _ = self.Actor.sample_normal(state)
+        self.Actor.train()
+        return action.cpu().detach().numpy()
+
+    def store_transition(self, state, action, reward, next_state, done):
+        self.memory.store_transition(state, action, reward, next_state, done)
+
+    def learn(self):
+        if self.memory.mem_counter < self.batch_size:
+            return
+
+        states, actions, rewards, next_states, done = self.memory.sample(
+            self.batch_size
+        )
+        states = torch.tensor(states).to(torch.float32).to(self.Actor.device)
+        next_states = torch.tensor(next_states).to(torch.float32).to(self.Actor.device)
+        actions = torch.tensor(actions).to(torch.float32).to(self.Actor.device)
+        rewards = torch.tensor(rewards).to(torch.float32).to(self.Actor.device)
+        done = torch.tensor(done).to(torch.bool).to(self.Actor.device)
+
+        self._value_loss(states)
+        self._actor_loss(states)
+
+        # get value of next state according to target network
+        next_state_values = self.V_target(next_states).view(-1)
+        next_state_values[done] = 0.0
+
+        # get critic values of states with old policy (from memory)
+        q1 = self.Q1(states, actions).view(-1)
+        q2 = self.Q2(states, actions).view(-1)
+
+        # calculate q_hat term i.e. scaled discounted returns
+        q_hat = self.reward_scale * rewards + self.gamma * (next_state_values)
+
+        # calculate critic loss
+        self.Q1.optimizer.zero_grad()
+        self.Q2.optimizer.zero_grad()
+        q1_loss = 0.5 * torch.nn.functional.mse_loss(q1, q_hat)
+        q2_loss = 0.5 * torch.nn.functional.mse_loss(q2, q_hat)
+        critic_loss = q1_loss + q2_loss
+        critic_loss.backward()
+        self.Q1.optimizer.step()
+        self.Q2.optimizer.step()
+
+        self.update_network_params()
+
+    def _value_loss(self, states):
+        values = self.V(states).view(-1)
+
+        # get min critic value of states with current policy
+        current_policy, log_probs = self.Actor.sample_normal(states)
+        log_probs = log_probs.view(-1)
+        q1 = self.Q1(states, current_policy)
+        q2 = self.Q2(states, current_policy)
+        critic_value = torch.min(q1, q2).view(-1)
+
+        # calculate value loss
+        self.V.optimizer.zero_grad()
+        value_targets = critic_value - log_probs
+        value_loss = 0.5 * torch.nn.functional.mse_loss(values, value_targets)
+        value_loss.backward(retain_graph=True)  # avoid coupling between loss functions
+        self.V.optimizer.step()
+
+    def _actor_loss(self, states):
+        # get min critic value of states with new policy (reparameterized)
+        new_policy, log_probs = self.Actor.sample_normal(states)
+        log_probs = log_probs.view(-1)
+        q1 = self.Q1(states, new_policy)
+        q2 = self.Q2(states, new_policy)
+        critic_value = torch.min(q1, q2).view(-1)
+
+        # calculate actor loss
+        actor_loss = torch.mean(log_probs - critic_value)
+        self.Actor.optimizer.zero_grad()
+        actor_loss.backward(retain_graph=True)
+        self.Actor.optimizer.step()
+
+    def update_network_params(self, tau=None):
+        if tau is None:
+            tau = self.tau
+
+        value_params = dict(self.V.named_parameters())
+        target_value_params = dict(self.V_target.named_parameters())
+        for name in value_params:
+            value_params[name] = (
+                tau * value_params[name].clone()
+                + (1 - tau) * target_value_params[name].clone()
+            )
+        self.V_target.load_state_dict(value_params)
+
+    def save_checkpoints(self):
+        self.V.save_checkpoint()
+        self.V_target.save_checkpoint()
+        self.Q1.save_checkpoint()
+        self.Q2.save_checkpoint()
+        self.Actor.save_checkpoint()
+
+    def load_checkpoints(self):
+        self.V.load_checkpoint()
+        self.V_target.load_checkpoint()
+        self.Q1.load_checkpoint()
+        self.Q2.load_checkpoint()
+        self.Actor.load_checkpoint()
---
+++ main.py
@@ -0,0 +1,137 @@
+import gymnasium as gym
+import utils
+from agent import SACAgent
+import numpy as np
+import os 
+import warnings 
+from argparse import ArgumentParser
+import pandas as pd 
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+environments = [
+    "BipedalWalker-v3",
+    "Pendulum-v1",
+    "MountainCarContinuous-v0",
+    "Ant-v4",
+    "HalfCheetah-v4",
+    "Hopper-v4",
+    "Humanoid-v4",
+    "LunarLanderContinuous-v2",
+    "HumanoidStandup-v4",
+    "InvertedDoublePendulum-v4",
+    "InvertedPendulum-v4",
+    "Pusher-v4",
+    "Reacher-v4",
+    "Swimmer-v3",
+    "Walker2d-v4",
+]
+
+def run_sac(env_name, n_games=1000):
+    env = gym.make(env_name, render_mode="rgb_array")
+
+    agent = SACAgent(
+    env.observation_space.shape,
+    env.action_space,
+    tau=5e-3,
+    reward_scale=2,
+    batch_size=64,
+    )
+
+    best_score = env.reward_range[0]
+    history = []
+    metrics = []
+
+    for i in range(n_games):
+        state, _ = env.reset()
+
+        term, trunc, score = False, False, 0
+        while not term and not trunc:
+            action = agent.choose_action(state)
+            next_state, reward, term, trunc, _ = env.step(action)
+
+            agent.store_transition(state, action, reward, next_state, term or trunc)
+            agent.learn()
+
+            score += reward
+            state = next_state
+
+        history.append(score)
+        avg_score = np.mean(history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_checkpoints()
+
+        metrics.append(
+            {
+                "episode": i + 1,
+                "score": score,
+                "average_score": avg_score,
+                "best_score": best_score,
+            }
+        )
+
+        print(
+            f"[{env_name} Episode {i + 1:04}/{n_games}]    Score = {score:7.4f}    Average = {avg_score:7.4f}",
+            end="\r",
+        )
+
+    return history, metrics, best_score, agent
+
+
+def save_best_version(env_name, agent, seeds=100):
+    agent.load_checkpoints()
+    
+    best_total_reward = float("-inf")
+    best_frames = None
+
+    for _ in range(seeds):
+        env = gym.make(env_name, render_mode="rgb_array")
+
+        frames = []
+        total_reward = 0
+
+        state, _ = env.reset()
+        term, trunc = False, False
+        while not term and not trunc:
+            frames.append(env.render())
+            action = agent.choose_action(state)
+            next_state, reward, term, trunc, _ = env.step(action)
+            state = next_state
+            total_reward += reward
+
+        if total_reward > best_total_reward:
+            best_total_reward = total_reward
+            best_frames = frames
+
+    utils.save_animation(best_frames, f"environments/{env_name}.gif")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-e", "--env", default=None, help="Environment name from Gymnasium"
+    )
+    parser.add_argument(
+        "-n", "--n_games", default=10000, type=int, help="Number of episodes (games) to run during training"
+    )
+    args = parser.parse_args()
+
+    for fname in ["metrics", "environments", "weights"]:
+        if not os.path.exists(fname):
+            os.makedirs(fname)
+
+    if args.env:
+        history, metrics, best_score, trained_agent = run_sac(args.env, args.n_games)
+        utils.plot_running_avg(history, args.env)
+        df = pd.DataFrame(metrics)
+        df.to_csv(f"metrics/{args.env}_metrics.csv", index=False)
+        save_best_version(args.env, trained_agent)
+    else:
+        for env_name in environments:
+            history, metrics, best_score, trained_agent = run_sac(env_name, args.n_games)
+            utils.plot_running_avg(history, env_name)
+            df = pd.DataFrame(metrics)
+            df.to_csv(f"metrics/{env_name}_metrics.csv", index=False)
+            save_best_version(env_name, trained_agent)
